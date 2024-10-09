690f317f5bd50a038bda59459047539377c65031
Alex Santos <1984alexsantos@gmail.com>
2024-10-09 15:13:07 -0300

    adicionado

---
+++ index.js
@@ -0,0 +1,10 @@
+var btnClique = document.getElementById("btnClique");
+
+btnClique.addEventListener("click", function() {
+
+    var conteudoBtn = document.getElementById("conteudoBtn");
+
+    conteudoBtn.classList.toggle("hide");
+    
+
+});
